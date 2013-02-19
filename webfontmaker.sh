fadf8e0309556eff82c0590c4046d963b4d9b301
mekka blue <mekka@mekkablue.com>
2013-02-19 19:11:23 +0100

    Added DSIG injection
    New DSIG support: injects a dummy DSIG into OTFs and TTFs, so OT
    features will work in MS Word. Needs an additional installation as
    described in the README.

--- webfontmaker.sh
+++ webfontmaker.sh
@@ -5,6 +5,8 @@
 rm *.html
 rm *.css
 rm *.eot
+rm *.ttx
+rm *.ttf
 
 # Check if a version number was passed as argument:
 if [[ $1 ]]; then
@@ -24,10 +26,9 @@ sed 's/x.xxx/$version/' $metadataSRC > $metadata
 # unfortunately, fontforge -c seems to be broken...?
 printf 'Open($1)\nGenerate($1:r + ".svg")\nScaleToEm(2048)\nRoundToInt()\nGenerate($1:r + ".ttf")' > makeweb.pe
 
-# Create the DSIG XML for later,
-# echo '<?xml version="1.0" encoding="ISO-8859-1"?>\n<ttFont sfntVersion="\\x00\\x01\\x00\\x00" ttLibVersion="2.2">\n<DSIG>\n   <hexdata>\n     00000001 00000000\n   </hexdata>\n</DSIG>\n</ttFont>\n' > dsig.ttx
-#
-# Currently disabled because of a bug in fontTools rev 612.
+# Create the DSIG XML for later:
+echo '<?xml version="1.0" encoding="ISO-8859-1"?>\n<ttFont sfntVersion="\\x00\\x01\\x00\\x00" ttLibVersion="2.2">\n<DSIG>\n   <hexdata>\n     00000001 00000000\n   </hexdata>\n</DSIG>\n</ttFont>\n' > dsig.ttx
+
 
 # Process all OTFs in the folder:
 for file in *.otf; do
@@ -36,10 +37,10 @@ for file in *.otf; do
 	# and calculate the other names:
 	basename=`echo "$file" | sed -e "s/\.otf//"`
 	otfFont="$basename.otf"
-	ttfFont="$basename-unhinted.ttf"
-	ttfAHFont="$basename.ttf"
-	eotFont="$basename-unhinted.eot"
-	eotAHFont="$basename.eot"
+	ttfFont="$basename.ttf"
+	ttfAHFont="$basename-autohinted.ttf"
+	eotFont="$basename.eot"
+	eotAHFont="$basename-autohinted.eot"
 	woffFont="$basename.woff"
 	svgFont="$basename.svg"
 
@@ -47,43 +48,55 @@ for file in *.otf; do
 	echo Processing $basename ...
 	
 	# Make SVG and TTF:
+	echo
 	echo Creating $ttfFont ...
 	fontforge -script makeweb.pe $otfFont
 	
 	# Fix SVG files:
+	echo
 	echo Creating $svgFont ...
 	sed '/^Created by .*$/d' $svgFont > tmp.svg; mv tmp.svg $svgFont
 	sed 's/^<svg>/<svg xmlns="http:\/\/www.w3.org\/2000\/svg">/' $svgFont > tmp.svg; mv tmp.svg $svgFont
 	
 	# Autohint TTF:
+	echo
 	echo Autohinting $ttfFont ...
 	ttfautohint $ttfFont $ttfAHFont
 	
 	# Make EOT:
+	echo
 	echo Creating $eotFont ...
 	java -jar ~/Applications/svn/sfntly-read-only/java/dist/tools/sfnttool/sfnttool.jar -e -x $ttfFont $eotFont
 	
+	echo
 	echo Autohinting $eotFont ...
 	java -jar ~/Applications/svn/sfntly-read-only/java/dist/tools/sfnttool/sfnttool.jar -e -x $ttfAHFont $eotAHFont
 
 	# make WOFF and its HTML files:
+	echo
 	echo Creating $woffFont ...
 	sfnt2woff -v $version -m $metadata $otfFont
 	woff-all $woffFont
 	
 	# Inject a Dummy DSIG table into the OTF & the TTF so they'll work in MS Word.
 	# Source: http://typedrawers.com/discussion/192/making-otttf-layout-features-work-in-ms-word-2010
-	#
-	# ttx -m $otfFont dsig.ttx
-	# ttx -m $ttfFont dsig.ttx
-	# ttx -m $ttfAHFont dsig.ttx
-	#
-	# Currently disabled because of a bug in fontTools rev 612.
+	# There's a bug in the current svn revision (612), so you need the latest stable release 2.3 (580):
+	# svn co -r 580 https://fonttools.svn.sourceforge.net/svnroot/fonttools /Applications/fonttools/
+	echo
+	echo Injecting DSIG into $otfFont, $ttfFont and $ttfAHFont ...
+	ttx -m $otfFont dsig.ttx
+	rm $otfFont; mv dsig.ttf $otfFont
+	
+	ttx -m $ttfFont dsig.ttx
+	rm $ttfFont; mv dsig.ttf $ttfFont
+	
+	ttx -m $ttfAHFont dsig.ttx
+	rm $ttfAHFont; mv dsig.ttf $ttfAHFont
 done
 
 # Clean up:
 rm $metadata
 rm makeweb.pe
-# rm dsig.ttx
+rm *.ttx
 
 echo
